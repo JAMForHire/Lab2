2a097d87c4e68552bd2b4a023bc3f3f98d95b314
john484848 <kloepj@rpi.edu>
2021-09-20 00:35:08 -0400

    Basic Card Animation
    Basic card animation css and javascript most of it is based on classes and should be pretty modular its not quite finished because there are some issues with text shifting

---
+++ main.js
@@ -0,0 +1,8 @@
+function flip1(card){
+  document.getElementById(card).classList.add("flip-card-inner2")
+
+}
+function flip2(card){
+  document.getElementById(card).classList.remove("flip-card-inner2")
+
+}
