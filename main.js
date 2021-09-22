4f2624842cf7db78aa1f3c125de9311b6b1b536c
simar-singh <simar.singh.r@gmail.com>
2021-09-22 17:20:02 -0400

    Corrected Card Text
    Fixed card text placement due to margins in the h2 element. After adding the appropriate styling, the text is in the right place.
    
    fix  #2

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 function flip1(card){
-  document.getElementById(card).classList.add("flip-card-inner2")
-
+  document.getElementById(card).classList.add("flip-card-inner2");
 }
-function flip2(card){
-  document.getElementById(card).classList.remove("flip-card-inner2")
 
+function flip2(card){
+  document.getElementById(card).classList.remove("flip-card-inner2");
 }
