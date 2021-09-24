af4da21d50043b47645244be7f4f9d1f84b81308
john484848 <kloepj@rpi.edu>
2021-09-23 23:13:35 -0400

    Comments Main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 function flip1(card){
   document.getElementById(card).classList.add("inner2");
+  //Flips the card to the analysis side
   document.getElementById(card).scrollIntoView();
+  //Makes it so it scrolls to the top when you click the flip button
 }
 
 function flip2(card){
   document.getElementById(card).classList.remove("inner2");
+  //Flips the card to the constitution text side.
   document.getElementById(card).scrollIntoView();
+  //Makes it so it scrolls to the top when you click the flip button
 }
