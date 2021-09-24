f1c2896bd5fa8d40fda11638618c00309d4eca26
simar-singh <simar.singh.r@gmail.com>
2021-09-23 23:06:14 -0400

    Fixed Scroll
    When the cards used to flip, it would not scroll to the top. Now it will scroll to the top after the card is flipped to create a continuation of reading for the user.

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function flip1(card){
-  document.getElementById(card).classList.add("flip-card-inner2");
+  document.getElementById(card).classList.add("inner2");
+  document.getElementById(card).scrollIntoView();
 }
 
 function flip2(card){
-  document.getElementById(card).classList.remove("flip-card-inner2");
+  document.getElementById(card).classList.remove("inner2");
+  document.getElementById(card).scrollIntoView();
 }
